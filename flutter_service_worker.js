2a0d6da155140ef8f497b4911136e65683c07977
fearda agnessiya <24111814138@mhs.unesa.ac.id>
2025-10-22 09:51:41 +0700

    deploy web fixed base href

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "7d26249b29cc726e0205013220d573c1
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "a4889d00c56b86fb35b9de066c9eae5c",
+"flutter_bootstrap.js": "2c6089df804d27b3952fefea8be3f911",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
